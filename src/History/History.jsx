2b89dfec9f32c9117c5e5736ac1b6d99e1bca71a
Magaly <maunigarropa@cesde.net>
2023-04-12 08:50:26 -0500

    cambios

--- src/History/History.jsx
+++ src/History/History.jsx
@@ -9,8 +9,18 @@ export function History(){
                     <p>Doctor Krápula es una banda de Rock mestizo colombiana, conformado por Mario Muñoz (Subcantante), Niko Cabrera, Germán Martínez, Sergio Acosta y David Jaramillo. Fusionan los ritmos de Punk, Ska, Cumbia, Hip-Hop y Reggae con una postura social, política y ambiental.Es una banda masiva e independiente con 25 años de historia, más de 700 conciertos en más de 15 países, que cuenta con 10 álbumes de estudio, un DVD en vivo, 3 Eps, numerosos sencillos y un proyecto para niños. Ha liderado y colaborado en diversos proyectos musicales y sociales junto a artistas como Manu Chao, Ska-P, SEEED, Juanes, Café Tacvba, entre otros.En sus 25 años de carrera, los Doctor Krápula han recorrido una gran parte del mundo, países como Venezuela, Ecuador, Argentina, México, Costa Rica, Estados Unidos, España, Alemania, Holanda, Bélgica, Italia, República Checa, Suiza, Dinamarca, Austria han sido parte de sus giras.</p>
                     <img src='https://firebasestorage.googleapis.com/v0/b/music-8bca7.appspot.com/o/drkrapula1.jpg?alt=media&token=7b3092fe-b7c0-4d4c-a76a-4468259b54a4' alt='' className='img-fluid w-100'></img>
                 </div>
-                <div className='col-12 col-md-6'></div>
-                <div className='col-12 col-md-6'></div>
+                <div className='col-12 col-md-6'>
+                <h2>Origenes</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum neque esse dicta, aperiam quas id sapiente sequi, eos maiores laudantium dolor? Repellendus quasi libero minus ratione excepturi exercitationem quo nemo.
+                    Numquam aliquid architecto, exercitationem voluptatum, quaerat ratione quam, pariatur natus minima deserunt eveniet accusamus voluptatem? Libero cupiditate deserunt eum porro nemo iure doloribus facere repellendus tempora rerum! Soluta, voluptas ea.</p>
+                    <img src='https://firebasestorage.googleapis.com/v0/b/music-8bca7.appspot.com/o/drkrapula2.jpg?alt=media&token=2d595e28-c834-4b4c-b50a-831b43ccd78b' alt='' className='img-fluid w-100'></img>
+                </div>
+                <div className='col-12 col-md-6'>
+                <h2>Trayetoria</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum neque esse dicta, aperiam quas id sapiente sequi, eos maiores laudantium dolor? Repellendus quasi libero minus ratione excepturi exercitationem quo nemo.
+                    Numquam aliquid architecto, exercitationem voluptatum, quaerat ratione quam, pariatur natus minima deserunt eveniet accusamus voluptatem? Libero cupiditate deserunt eum porro nemo iure doloribus facere repellendus tempora rerum! Soluta, voluptas ea.</p>
+                    <img src='https://firebasestorage.googleapis.com/v0/b/music-8bca7.appspot.com/o/drkrapula2.jpg?alt=media&token=2d595e28-c834-4b4c-b50a-831b43ccd78b' alt='' className='img-fluid w-100'></img>
+                </div>
             </div>
             
         </div>
